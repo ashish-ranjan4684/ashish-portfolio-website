b7e8b35ae5411a1a53b174e4c7875766e0d2d360
ashish-ranjan4684 <ashish.ranjan4684@gmail.com>
2026-07-13 23:46:52 +0530

    .

--- routes/login.js
+++ routes/login.js
@@ -19,6 +19,7 @@ const authClient = new proto.AuthService(
 router.post("/login", (req, res) => {
 
     const { email, password } = req.body;
+    console.log(email,password);
 
     if (!email || !password) {
         return res.status(400).json({
